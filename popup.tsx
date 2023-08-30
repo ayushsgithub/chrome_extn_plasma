05bcd3cd8ed42f3d78cbccf484da1a0bec28a1ff
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-08-30 12:36:33 +0530

    fetch function added

--- popup.tsx
+++ popup.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react" 
+import { useEffect, useState } from "react" 
 import "./style.css"
 import axios from "axios"
  
@@ -8,25 +8,28 @@ function IndexPopup() {
   const [loading, setLoading] = useState(false)
   const [ip, setIp] = useState()
 
-  const handlefetch = async () => {
-    setLoading(true)
-    setButton(false)
-    setTimeout(() => {
-      setButton(true)
-      setLoading(false)
-    }, 3000);
+  const handleFetch = async () => {
+    setLoading(true);
+    setButton(false);
 
-    const response = await axios.get('http://api.ipify.org');
-    const ip = response.data;
-    setIp(ip)
-  }
+    try {
+      const response = await axios.get('https://api.ipify.org?format=json');
+      setIp(response.data.ip);
+    } catch (error) {
+      console.error('Error fetching IP:', error);
+    }
+
+    setLoading(false);
+    setButton(true);
+  };
+
 
   return (
-    <div className="w-[500px] h-[500px] bg-blue-500 flex flex-col space-y-6 justify-center items-center relative">
+    <div className="w-[500px] h-[500px] bg-gray-400 flex flex-col space-y-6 justify-center items-center relative">
       <div className="text-white absolute top-28 text-2xl">
       {button ? `Your IP address is ${ip}`: ""} 
       </div>
-      <button onClick={handlefetch} className="bg-blue-700 text-xl rounded-full p-4 text-white hover:bg-blue-800">
+      <button onClick={handleFetch} className="bg-gradient-to-b from-blue-600 to-blue-700 text-xl rounded shadow-xl p-4 text-white  hover:bg-blue-800">
         {loading ? "Loading..." : "Show My Location"}
       </button>
     </div>
